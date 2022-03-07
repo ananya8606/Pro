c0c4266f5ca3177d6b054f1d9e8275962ecdc96c
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2022-03-07 09:42:50 +0530

    Update users.js

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,8 +2,8 @@ import bcrypt from 'bcryptjs'
 
 const users = [
   {
-    name: 'Admin User',
-    email: 'admin@example.com',
+    name: 'Admin',
+    email: 'gupta@example.com',
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
