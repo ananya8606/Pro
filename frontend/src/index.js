17731f7d16ee1389b0b317a2b96f46ae2e09aa5a
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2022-05-07 10:57:16 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
 // import 'bootstrap/dist/css/bootstrap.min.css'
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
